3bf1b0d2fa06d03d63c93ae5b87c9e49eaca9e88
ALL-MIGHTY-LABT\karel <mirakl834@gmail.com>
2018-02-24 08:37:35 -0500

    changed where importing the StatusBar

--- App.js
+++ App.js
@@ -1,24 +1,22 @@
-import Expo from 'expo';
-import React from 'react';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import firebase from 'firebase';
-import ReduxThunk from 'redux-thunk';
-import reducers from './reducers';
-import { StyleSheet, Text, View, Platform } from 'react-native';
-import { StackNavigator, TabNavigator } from 'react-navigation';
-import NavigatorService from './utils/navigator';
-import {
-  RkTheme
-} from 'react-native-ui-kitten';
+import Expo from "expo";
+import React from "react";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import firebase from "firebase";
+import ReduxThunk from "redux-thunk";
+import reducers from "./reducers";
+import { StyleSheet, Text, View, Platform, StatusBar } from "react-native";
+import { StackNavigator, TabNavigator } from "react-navigation";
+import NavigatorService from "./utils/navigator";
+import { RkTheme } from "react-native-ui-kitten";
 
-import Welcome_Screen from './screens/Welcome_Screen';
-import Profile_Screen from './screens/Profile_Screen';
-import Login_Screen from './screens/Login_Screen';
-import Register_Screen from './screens/Register_Screen';
-import Reset_Screen from './screens/Reset_Screen';
-import Menu_Screen from './screens/Menu_Screen';
-import QrScan_Screen from './screens/QrScan_Screen';
+import Welcome_Screen from "./screens/Welcome_Screen";
+import Profile_Screen from "./screens/Profile_Screen";
+import Login_Screen from "./screens/Login_Screen";
+import Register_Screen from "./screens/Register_Screen";
+import Reset_Screen from "./screens/Reset_Screen";
+import Menu_Screen from "./screens/Menu_Screen";
+import QrScan_Screen from "./screens/QrScan_Screen";
 
 export default class App extends React.Component {
   constructor(props) {
@@ -26,7 +24,6 @@ export default class App extends React.Component {
     this.store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
   }
   componentWillMount() {
-
     firebase.initializeApp({
       apiKey: "AIzaSyCzt6SefehE6JmmQdTaZ0t9B2DfyvwJC9k",
       authDomain: "ktaa-13a11.firebaseapp.com",
@@ -35,62 +32,64 @@ export default class App extends React.Component {
       storageBucket: "ktaa-13a11.appspot.com",
       messagingSenderId: "873334685017"
     });
-
   }
   render() {
-
-    const MainNavigator = TabNavigator({
-      menu_scr: { screen: Menu_Screen },
-      qr_scan: { screen: QrScan_Screen }
-      // orders_screen: { screen: Orders_Screen },
-      // settings_screen: { screen: Settings_Screen },
-    },
+    const MainNavigator = TabNavigator(
+      {
+        menu_scr: { screen: Menu_Screen },
+        qr_scan: { screen: QrScan_Screen }
+        // orders_screen: { screen: Orders_Screen },
+        // settings_screen: { screen: Settings_Screen },
+      },
       {
         navigationOptions: {
           headerLeft: null,
           headerStyle: {
-            backgroundColor: 'white',
+            backgroundColor: "white",
             elevation: 2,
-            paddingTop: Platform.OS === 'ios' ? 0 : StatusBar.currentHeight + 10
+            paddingTop: Platform.OS === "ios" ? 0 : StatusBar.currentHeight + 10
           },
           headerTitleStyle: {
             fontSize: RkTheme.current.fonts.sizes.h5,
-            alignSelf: 'center',
-            marginBottom: Platform.OS === 'ios' ? 0 : 10,
-            marginTop: Platform.OS === 'ios' ? 25 : 0
+            alignSelf: "center",
+            marginBottom: Platform.OS === "ios" ? 0 : 10,
+            marginTop: Platform.OS === "ios" ? 25 : 0
           }
         },
         tabBarOptions: {
           showLabel: false,
           showIcon: true,
-          indicatorStyle: { backgroundColor: '#ffffff' },
+          indicatorStyle: { backgroundColor: "#ffffff" },
           activeTintColor: RkTheme.current.colors.accent,
           inactiveTintColor: RkTheme.current.colors.text.hint,
-          style: { backgroundColor: '#ffffff' },
+          style: { backgroundColor: "#ffffff" }
         },
         cardStyle: {
-          paddingTop: Platform.OS === 'ios' ? 0 : StatusBar.currentHeight
+          paddingTop: Platform.OS === "ios" ? 0 : StatusBar.currentHeight
         },
         swipeEnabled: false,
-        tabBarPosition: 'bottom',
-      })
+        tabBarPosition: "bottom"
+      }
+    );
 
-    const LoginNavigator = StackNavigator({
-      welcome_screen: { screen: Welcome_Screen },
-      register_screen: { screen: Register_Screen },
-      reset_screen: { screen: Reset_Screen },
-      profile_screen: { screen: Profile_Screen },
-      login_screen: { screen: Login_Screen },
-      main_screen: { screen: MainNavigator },
-      qr_scan: { screen: QrScan_Screen }
-    },
+    const LoginNavigator = StackNavigator(
+      {
+        welcome_screen: { screen: Welcome_Screen },
+        register_screen: { screen: Register_Screen },
+        reset_screen: { screen: Reset_Screen },
+        profile_screen: { screen: Profile_Screen },
+        login_screen: { screen: Login_Screen },
+        main_screen: { screen: MainNavigator },
+        qr_scan: { screen: QrScan_Screen }
+      },
       {
         navigationOptions: {
           tabBarVisible: false
         },
         swipeEnabled: false,
         lazy: true
-      });
+      }
+    );
 
     return (
       <Provider store={this.store}>
@@ -98,7 +97,8 @@ export default class App extends React.Component {
           <LoginNavigator
             ref={navigatorRef => {
               NavigatorService.setContainer(navigatorRef);
-            }} />
+            }}
+          />
         </View>
       </Provider>
     );
@@ -108,7 +108,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    justifyContent: "center"
+  }
 });
--- screens/Menu_Screen.js
+++ screens/Menu_Screen.js
@@ -1,24 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   Image,
   Text,
   View,
   TouchableOpacity,
   Platform,
-  StatusBar,
   Button
-} from 'react-native';
-import {
-  RkText,
-  RkCard,
-  RkStyleSheet,
-  RkTheme
-} from 'react-native-ui-kitten';
-import { Header } from 'react-navigation';
-import NavigatorService from './../utils/navigator';
+} from "react-native";
+import { RkText, RkCard, RkStyleSheet, RkTheme } from "react-native-ui-kitten";
+import { Header } from "react-navigation";
+import NavigatorService from "./../utils/navigator";
 
 class Menu_Screen extends Component {
-
   constructor(props) {
     super(props);
   }
@@ -29,18 +22,17 @@ class Menu_Screen extends Component {
         {/* <RkText>
           Guess who has 2 thumbs and is logged in.... YOU :D
         </RkText> */}
-        <Button title="QR" onPress={() => NavigatorService.reset('qr_scan')} />
+        <Button title="QR" onPress={() => NavigatorService.reset("qr_scan")} />
       </View>
-    )
+    );
   }
 }
-
 
 let styles = RkStyleSheet.create(theme => ({
   container: {
     backgroundColor: theme.colors.screen.scroll,
-    justifyContent: 'center'
-  },
+    justifyContent: "center"
+  }
 }));
 
 export default Menu_Screen;
