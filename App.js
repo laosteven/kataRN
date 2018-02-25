a5905304447aebe8dab2bd7a1cbc5c3397204c3c
ALL-MIGHTY-LABT\karel <mirakl834@gmail.com>
2018-02-25 08:16:52 -0500

    fixed replicated longin navigator

--- App.js
+++ App.js
@@ -1,19 +1,26 @@
-
-import Expo from 'expo';
-import React from 'react';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import firebase from 'firebase';
-import ReduxThunk from 'redux-thunk';
-import reducers from './reducers';
-import { StyleSheet, Text, View, StatusBar, Platform, Button, TouchableOpacity } from 'react-native';
-import { StackNavigator, TabNavigator } from 'react-navigation';
-import NavigatorService from './utils/navigator';
-import { RkTheme, RkButton, RkStyleSheet } from 'react-native-ui-kitten';
-import { bootstrap } from './style/themeBootstrapper';
-import { UtilStyles } from './style/styles'; 
-import { AppLoading, Font } from 'expo';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import Expo from "expo";
+import React from "react";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import firebase from "firebase";
+import ReduxThunk from "redux-thunk";
+import reducers from "./reducers";
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  Platform,
+  Button,
+  TouchableOpacity
+} from "react-native";
+import { StackNavigator, TabNavigator } from "react-navigation";
+import NavigatorService from "./utils/navigator";
+import { RkTheme, RkButton, RkStyleSheet } from "react-native-ui-kitten";
+import { bootstrap } from "./style/themeBootstrapper";
+import { UtilStyles } from "./style/styles";
+import { AppLoading, Font } from "expo";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 import Welcome_Screen from "./screens/Welcome_Screen";
 import Profile_Screen from "./screens/Profile_Screen";
@@ -68,49 +75,65 @@ export default class App extends React.Component {
     if (!this.state.loaded) {
       return <AppLoading />;
     }
-    
-    const MainNavigator = TabNavigator({
-      menu_scr: { 
-        screen: Menu_Screen, 
-        title: "Kata",
-        navigationOptions: {
-            tabBarIcon: ({ tintColor }) => <Icon name={'train'} size={20} color={tintColor} />
+
+    const MainNavigator = TabNavigator(
+      {
+        menu_scr: {
+          screen: Menu_Screen,
+          title: "Kata",
+          navigationOptions: {
+            tabBarIcon: ({ tintColor }) => (
+              <Icon name={"train"} size={20} color={tintColor} />
+            )
+          }
+        },
+        board_scan: {
+          screen: QrScan_Screen,
+          title: "Board",
+          navigationOptions: {
+            tabBarIcon: ({ tintColor }) => (
+              <Icon name={"sign-in"} size={20} color={tintColor} />
+            )
+          }
+        },
+        waiting_room: {
+          screen: Waiting_Room_Screen,
+          title: "Waiting Room",
+          navigationOptions: {
+            tabBarIcon: ({ tintColor }) => (
+              <Icon name={"clock-o"} size={20} color={tintColor} />
+            )
+          }
+        },
+        travel: {
+          screen: Map_Screen,
+          title: "Travel",
+          navigationOptions: {
+            tabBarIcon: ({ tintColor }) => (
+              <Icon name={"map"} size={20} color={tintColor} />
+            )
+          }
+        },
+        deboard_scan: {
+          screen: QrScan_Screen_Deboard,
+          title: "Deboard",
+          navigationOptions: {
+            tabBarIcon: ({ tintColor }) => (
+              <Icon name={"sign-out"} size={20} color={tintColor} />
+            )
+          }
         }
       },
-      board_scan: { 
-        screen: QrScan_Screen, 
-        title: "Board",
-        navigationOptions: {
-            tabBarIcon: ({ tintColor }) => <Icon name={'sign-in'} size={20} color={tintColor} />
-        }
-      },
-      waiting_room: { 
-        screen: Waiting_Room_Screen, 
-        title: "Waiting Room",
-        navigationOptions: {
-            tabBarIcon: ({ tintColor }) => <Icon name={'clock-o'} size={20} color={tintColor} />
-        }
-      },
-      travel: { 
-        screen: Map_Screen, 
-        title: "Travel",
-        navigationOptions: {
-            tabBarIcon: ({ tintColor }) => <Icon name={'map'} size={20} color={tintColor} />
-        }
-      },
-      deboard_scan: { 
-        screen: QrScan_Screen_Deboard, 
-        title: "Deboard",
-        navigationOptions: {
-            tabBarIcon: ({ tintColor }) => <Icon name={'sign-out'} size={20} color={tintColor} />
-        }
-      }
-    },
       {
         navigationOptions: ({ navigation }) => ({
           headerRight: (
-            <TouchableOpacity onPress={() => NavigatorService.navigate('settings_screen')}>
-            <Icon style={[UtilStyles.icon, {fontSize: 24, marginRight: 10}]} name={'sliders'} />
+            <TouchableOpacity
+              onPress={() => NavigatorService.navigate("settings_screen")}
+            >
+              <Icon
+                style={[UtilStyles.icon, { fontSize: 24, marginRight: 10 }]}
+                name={"sliders"}
+              />
             </TouchableOpacity>
           ),
           tabBarOnPress: (scene, jumpToIndex) => {
@@ -118,23 +141,10 @@ export default class App extends React.Component {
           }
         }),
 
-        tabBarPosition: 'bottom',
-      })
-      
-    const LoginNavigator = StackNavigator(
-      {
-        welcome_screen: { screen: Welcome_Screen },
-        register_screen: { screen: Register_Screen },
-        reset_screen: { screen: Reset_Screen },
-        profile_screen: { screen: Profile_Screen },
-        login_screen: { screen: Login_Screen },
-        main_screen: { screen: MainNavigator },
-        qr_scan: { screen: QrScan_Screen },
-        settings_screen: { screen: Settings_Screen },
-        thank_you: { screen: ThankYou_Screen },
-        map_screen: { screen: Map_Screen }
+        tabBarPosition: "bottom"
       }
     );
+
     const LoginNavigator = StackNavigator({
       welcome_screen: { screen: Welcome_Screen },
       register_screen: { screen: Register_Screen },
@@ -144,7 +154,8 @@ export default class App extends React.Component {
       main_screen: { screen: MainNavigator },
       qr_scan: { screen: QrScan_Screen },
       settings_screen: { screen: Settings_Screen },
-      thank_you: { screen: ThankYou_Screen }
+      thank_you: { screen: ThankYou_Screen },
+      map_screen: { screen: Map_Screen }
     });
 
     return (
