1a34597479223b117706c94331909c9aa13ef4a9
ALL-MIGHTY-LABT\karel <mirakl834@gmail.com>
2018-02-24 20:28:42 -0500

    magic disapearing user marker

--- screens/Map_Screen.js
+++ screens/Map_Screen.js
@@ -11,113 +11,159 @@ import {
 import { RkText, RkCard, RkStyleSheet, RkTheme } from "react-native-ui-kitten";
 import { Header } from "react-navigation";
 import NavigatorService from "./../utils/navigator";
-import { MapView, Circle, Constants, Location, Permissions } from "expo";
+import {
+  MapView,
+  AnimatedRegion,
+  Circle,
+  Constants,
+  Location,
+  Permissions
+} from "expo";
 
 class Map_Screen extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      region: {
+        latitude: 44.363518,
+        longitude: -76.360238,
+        latitudeDelta: 13,
+        longitudeDelta: 15
+      },
+      currentLocation: new MapView.AnimatedRegion({
+        latitude: 43.645268,
+        longitude: -79.380537
+      }),
+      markers: [
+        {
+          title: "Windsor",
+          coordinates: {
+            latitude: 42.324966,
+            longitude: -83.007179
+          }
+        },
+        {
+          title: "London",
+          coordinates: {
+            latitude: 42.981506,
+            longitude: -81.247084
+          }
+        },
+        {
+          title: "Kitchener-Waterloo",
+          coordinates: {
+            latitude: 43.455635,
+            longitude: -80.493136
+          }
+        },
+        {
+          title: "Pearson Airport",
+          coordinates: {
+            latitude: 43.678123,
+            longitude: -79.624995
+          }
+        },
+        {
+          title: "Toronto Union",
+          coordinates: {
+            latitude: 43.645268,
+            longitude: -79.380537
+          }
+        },
+        {
+          title: "Toronto East Harbour Transit Hub",
+          coordinates: {
+            latitude: 43.656494,
+            longitude: -79.345338
+          }
+        },
+        {
+          title: "Kingston",
+          coordinates: {
+            latitude: 44.257619,
+            longitude: -76.536476
+          }
+        },
+        {
+          title: "Ottawa",
+          coordinates: {
+            latitude: 45.416327,
+            longitude: -75.651603
+          }
+        },
+        {
+          title: "Montreal",
+          coordinates: {
+            latitude: 45.499983,
+            longitude: -73.566643
+          }
+        },
+        {
+          title: "Quebec",
+          coordinates: {
+            latitude: 46.817582,
+            longitude: -71.214163
+          }
+        }
+      ]
+    };
   }
 
-  state = {
-    region: {
-      latitude: 44.363518,
-      longitude: -76.360238,
-      latitudeDelta: 13,
-      longitudeDelta: 15
-    },
-    markers: [
-      {
-        title: "Windsor",
-        coordinates: {
-          latitude: 42.324966,
-          longitude: -83.007179
-        }
-      },
-      {
-        title: "London",
-        coordinates: {
-          latitude: 42.981506,
-          longitude: -81.247084
-        }
-      },
-      {
-        title: "Kitchener-Waterloo",
-        coordinates: {
-          latitude: 43.455635,
-          longitude: -80.493136
-        }
-      },
-      {
-        title: "Pearson Airport",
-        coordinates: {
-          latitude: 43.678123,
-          longitude: -79.624995
-        }
-      },
-      {
-        title: "Toronto Union",
-        coordinates: {
-          latitude: 43.645268,
-          longitude: -79.380537
-        }
-      },
-      {
-        title: "Toronto East Harbour Transit Hub",
-        coordinates: {
-          latitude: 43.656494,
-          longitude: -79.345338
-        }
-      },
-      {
-        title: "Kingston",
-        coordinates: {
-          latitude: 44.257619,
-          longitude: -76.536476
-        }
-      },
-      {
-        title: "Ottawa",
-        coordinates: {
-          latitude: 45.416327,
-          longitude: -75.651603
-        }
-      },
-      {
-        title: "Montreal",
-        coordinates: {
-          latitude: 45.499983,
-          longitude: -73.566643
-        }
-      },
-      {
-        title: "Quebec",
-        coordinates: {
-          latitude: 46.817582,
-          longitude: -71.214163
-        }
+  componentWillMount() {
+    let lat = 42.324966;
+    let long = -83.007179;
+
+    this._setCurrentLocation(lat, long);
+  }
+
+  _setCurrentLocation = (x, y) => {
+    this.setState({
+      currentLocation: {
+        latitude: x,
+        longitude: y
       }
-    ]
+    });
+  };
+
+  animate = () => {
+    this.setState({
+      currentLocation: {
+        latitude: 42.981506,
+        longitude: -81.247084
+      }
+    });
   };
 
   render() {
     return (
-      <MapView style={styles.map} initialRegion={this.state.region}>
-        {this.state.markers.map((marker, index) => (
-          <MapView.Marker
-            key={index}
-            coordinate={marker.coordinates}
-            title={marker.title}
+      <View style={styles.container}>
+        <MapView style={styles.map} initialRegion={this.state.region}>
+          {this.state.markers.map((marker, index) => (
+            <MapView.Marker
+              key={index}
+              coordinate={marker.coordinates}
+              title={marker.title}
+            />
+          ))}
+          <MapView.Marker.Animated
+            ref={userMarker => {
+              _userMarker = userMarker;
+            }}
+            coordinate={this.state.currentLocation}
+            title="You"
+            pinColor="#0000FF"
           />
-        ))}
-        <MapView.Marker
-          coordinate={{
-            latitude: 43.645268,
-            longitude: -79.380537
-          }}
-          title="You"
-          pinColor="#0000FF"
-        />
-      </MapView>
+        </MapView>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            onPress={this.animate}
+            style={[styles.bubble, styles.button]}
+          >
+            <Text>Animate</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     );
   }
 }
@@ -144,6 +190,24 @@ let styles = RkStyleSheet.create(theme => ({
     top: 0,
     bottom: 0,
     position: "absolute"
+  },
+  bubble: {
+    flex: 1,
+    backgroundColor: "rgba(255,255,255,0.7)",
+    paddingHorizontal: 18,
+    paddingVertical: 12,
+    borderRadius: 20
+  },
+  button: {
+    width: 80,
+    paddingHorizontal: 12,
+    alignItems: "center",
+    marginHorizontal: 10
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    marginVertical: 20,
+    backgroundColor: "transparent"
   }
 }));
 
